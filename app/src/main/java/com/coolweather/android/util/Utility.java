79abb4672696388b32ccd5cc5aed9f6d5ada4cdb
Chenxinwei <523750037@qq.com>
2017-12-06 09:53:23 +0800

    2nd commit

---
+++ app/src/main/java/com/coolweather/android/util/Utility.java
@@ -0,0 +1,12 @@
+package com.coolweather.android.util;
+
+import android.text.TextUtils;
+
+import org.w3c.dom.Text;
+
+public class Utility {
+    public static boolean headleProvinceResponse(String response){
+        if (!TextUtils.isEmpty(response))
+        return false;
+    }
+}
